67e9421d93544800511a16c60d33a60ca01f35f3
asathe <asathe@mit.edu>
2024-02-26 10:29:20 -0500

    Update find_device.m

--- find_device.m
+++ find_device.m
@@ -1,20 +1,21 @@
 function dev_idx = find_device(target)
     arguments
-        % target = 'Current Devices';
-        target = 'Microsoft Natural';
+        target = 'Current Designs';
+        % target = 'Microsoft Natural';
     end
 
+    dev_idx = -1;
     % device class '4' corresponds to keyboards
     devs = PsychHID('Devices', 4);
     
     for idx = 1:length(devs)
         prod = devs(idx).product;
-        dev_idx = devs(idx).index;
-        fprintf('\nproduct: %s; index: %d\n', prod, dev_idx);
+        dev_idx_ = devs(idx).index;
+        fprintf('\nproduct: %s; index: %d\n', prod, dev_idx_);
         
         if contains(prod, target)
-            % disp(prod);
-            break;
+            dev_idx = dev_idx_;
+            % break;
         end
     end
 end
