f956025f625ea67d24ba93a885ee8b0a40b9b2b1
asathe <asathe@mit.edu>
2024-03-05 13:57:40 -0500

    allow using native keyboard (AT translated set) in case the trigger interface in the scanning room is not found

--- find_device.m
+++ find_device.m
@@ -1,21 +1,39 @@
 function dev_idx = find_device(target)
     arguments
         target = 'Current Designs';
-        % target = 'Microsoft Natural';
     end
 
-    dev_idx = -1;
+    alt_target = 'AT Translated Set 2';
+
     % device class '4' corresponds to keyboards
     devs = PsychHID('Devices', 4);
-    
+    dev_idx = -1;
+    dev_idx_alt = -1;
+    prod = -1;
+
+
     for idx = 1:length(devs)
-        prod = devs(idx).product;
+        prod_ = devs(idx).product;
         dev_idx_ = devs(idx).index;
-        fprintf('\nproduct: %s; index: %d\n', prod, dev_idx_);
+        fprintf('\nproduct: %s; index: %d\n', prod_, dev_idx_);
         
-        if contains(prod, target)
+        if contains(prod_, target)
             dev_idx = dev_idx_;
             % break;
         end
+        if contains(prod_, alt_target)
+            dev_idx_alt = dev_idx_;
+            % break;
+        end
     end
+
+    % if we didn't find the primary target, 
+    % try the secondary target (we are probably not
+    % in the scanner, and in debug mode)
+    if (dev_idx == -1)
+        dev_idx = dev_idx_alt;
+    end
+
+    fprintf('\nFINAL: product: %s; index: %d\n', dev_idx);
+
 end
