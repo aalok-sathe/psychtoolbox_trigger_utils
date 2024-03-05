beabaf003972c0d71b875848761a63d21bef1753
evlab-daly <fedorenkolanguagelab+daly@gmail.com>
2024-03-05 17:14:34 -0500

    timestamp git pulls using hook

---
+++ post-merge-timestamp.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# get current date and time
+now=$(date +"%Y-%m-%d--%H-%M")
+
+# remove any existing timestamp
+rm -f *.timestamp
+
+# create a new file with the current date and time in the title
+# and custom prefix
+touch "LAST_PULLED_AT_$now.timestamp"
+
